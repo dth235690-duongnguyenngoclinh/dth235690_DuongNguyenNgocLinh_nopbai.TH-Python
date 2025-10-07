80dc4924d41f933c6ee3b7b7bec5fe93d22fa0bd
dth235690-duongnguyenngoclinh <linh_dth235690@student.agu.edu.vn>
2025-10-07 09:32:02 +0700

    chuong3

--- BaiTapChuong3/Cau18.py
+++ BaiTapChuong3/Cau18.py
@@ -1,27 +1,52 @@
 n = int(input("Nhập n: "))
 
-
-print("Hình 1")
+# ---- Hình 1: hình vuông viền ----
+print("Hình 1:")
 for i in range(n):
     for j in range(n):
         if i == 0 or i == n - 1 or j == 0 or j == n - 1:
-            print('*', end=' ')
+            print('*', end='')
         else:
-            print(' ', end=' ')
+            print(' ', end='')
     print()
 
-print()  # Dòng trống giữa hai hình
+print()  # dòng trống giữa các hình
 
-
+# ---- Hình 2: tam giác rỗng đẩy sang phải ----
 print("Hình 2:")
 for i in range(n):
-    # In khoảng trắng để đẩy tam giác sang phải
-    for k in range(n - i - 1):
-        print(' ', end=' ')
-    # In phần tam giác
+    # in khoảng trắng để đẩy sang phải
+    print(' ' * (n - i - 1), end='')
+    # in tam giác rỗng (đầu, cạnh phải, hoặc hàng đáy đầy sao)
     for j in range(i + 1):
         if j == 0 or j == i or i == n - 1:
-            print('*', end=' ')
+            print('*', end='')
         else:
-            print(' ', end=' ')
+            print(' ', end='')
     print()
+
+print()
+
+# ---- Hình 3: hai tam giác rỗng đối xứng qua trục ngang ----
+print("Hình 3:")
+width = 2 * n - 1
+upper = []
+
+# phần trên (n-1 dòng)
+for i in range(1, n):
+    line = [' '] * width
+    line[0] = '*'                 # cạnh trái
+    if i > 1:
+        line[2 * (i - 1)] = '*'   # cạnh xiên phải (cách đều bằng 1 ô)
+    upper.append(''.join(line))
+
+# in phần trên
+for ln in upper:
+    print(ln)
+
+# dòng giữa đầy sao
+print('*' * width)
+
+# phần dưới: phản chiếu ngang phần trên
+for ln in reversed(upper):
+    print(ln[::-1])
